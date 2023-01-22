ecbb0f677f0d9ba834b4abd2d6dbfc4421e53b21
kenta yamada <pandorina463@gmail.com>
2023-01-23 00:40:11 +0900

    add todo

--- executor.sh
+++ executor.sh
@@ -9,10 +9,17 @@ if lspci -vnn | grep NVIDIA > /dev/null 2>&1; then
   fi
 fi
 
+# git clone
+
+# sync gcs bucket
+
+# execute experiment
+
+# upload result to gcs bucket
+
+# delete instance
 readonly INSTANCE_NAME=$(curl http://metadata.google.internal/computeMetadata/v1/instance/name -H "Metadata-Flavor: Google")
 INSTANCE_ZONE="/"$(curl http://metadata.google.internal/computeMetadata/v1/instance/zone -H "Metadata-Flavor: Google")
 INSTANCE_ZONE="${INSTANCE_ZONE##/*/}"
 readonly INSTANCE_PROJECT_NAME=$(curl http://metadata.google.internal/computeMetadata/v1/project/project-id -H "Metadata-Flavor: Google")
-
-# delete instance
 gcloud --quiet compute instances delete "${INSTANCE_NAME}" --zone "${INSTANCE_ZONE}" --project "${INSTANCE_PROJECT_NAME}"

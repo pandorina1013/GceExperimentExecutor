8f6c2ff1ab1896553632f976ed35d88c58983536
pandorina1013 <pandorina463@gmail.com>
2023-01-23 17:40:59 +0900

    change install nvidia driver place

--- executor.sh
+++ executor.sh
@@ -5,10 +5,10 @@ trap finally EXIT
 
 function catch {
   echo "Experiment failed."
-  gcloud --quiet compute instances delete "${INSTANCE_NAME}" --zone "${INSTANCE_ZONE}" --project "${INSTANCE_PROJECT_NAME}"
+  # gcloud --quiet compute instances delete "${INSTANCE_NAME}" --zone "${INSTANCE_ZONE}" --project "${INSTANCE_PROJECT_NAME}"
 }
 function finally {
-  echo "Experiment finished successfully, delete instance."
+  echo "Delete instance."
   gcloud --quiet compute instances delete "${INSTANCE_NAME}" --zone "${INSTANCE_ZONE}" --project "${INSTANCE_PROJECT_NAME}"
 }
 
@@ -17,31 +17,27 @@ readonly INSTANCE_NAME=$(curl http://metadata.google.internal/computeMetadata/v1
 INSTANCE_ZONE="/"$(curl http://metadata.google.internal/computeMetadata/v1/instance/zone -H "Metadata-Flavor: Google")
 INSTANCE_ZONE="${INSTANCE_ZONE##/*/}"
 readonly INSTANCE_PROJECT_NAME=$(curl http://metadata.google.internal/computeMetadata/v1/project/project-id -H "Metadata-Flavor: Google")
-readonly ENVIRONMENT=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/environment-setting -H "Metadata-Flavor: Google")
 readonly EXECUTE_FILE=$(curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/execute_file -H "Metadata-Flavor: Google")
 
-# install nvidia driver
-if lspci -vnn | grep NVIDIA > /dev/null 2>&1; then
-  if ! nvidia-smi > /dev/null 2>&1; then
-    echo "Installing driver"
-    /opt/deeplearning/install-driver.sh
-  fi
-fi
+# set environment
+curl http://metadata.google.internal/computeMetadata/v1/instance/attributes/environment-setting -H "Metadata-Flavor: Google" > .env
+export $(cat .env| grep -v "#" | xargs)
 
 # git clone
-export $(cat ${ENVIRONMENT} | grep -v ^\#)
-git clone https://${GIT_USER}:${GIT_PASS}@https://github.com/${GIT_USER}/${GIT_REPO}.git
+git clone https://${GIT_USER}:${GIT_TOKEN}@github.com/${GIT_USER}/${GIT_REPO}
 cd ${GIT_REPO}
 
 # set environment
-cat ${ENVIRONMENT} > .env
+mv ../.env .env
 
 # sync gcs bucket
-pip install -r requirements.txt --user
+pip3 install -r requirements.txt --user
 # python gcs_sync.py
 
 # execute experiment
-python EXECUTE_FILE
+# python EXECUTE_FILE
 
 # upload result to gcs bucket
 # python gcs_sync.py -u
+
+echo "Experiment finished successfully."
